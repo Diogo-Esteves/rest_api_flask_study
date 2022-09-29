4e3b5e9bd4e0f728fc17e1222079a0d274a00f08
Diogo Esteves dos Santos <diogoesteves_skt@hotmail.com>
2022-09-28 21:22:20 -0300

    adding a dockerfile and added infos in the readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # rest_api_flask_study
 Using this repo to study
+
+## Run locally
+
+```sh
+flask run
+```
+
+## Run locally with docker
+
+```sh
+docker build -t rest-api-flask .
+docker container run -p 5000:5000 rest-api-flask
+```
+
+## To run the tests
+
+```sh
+python -m unittest
+```
--- app.py
+++ app.py
@@ -15,6 +15,14 @@ def get_store():
     return {"stores": stores}
 
 
+@app.get("/store/<string:name>/item")
+def get_store_item(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store["items"]}
+    return {"message": "Store not found"}, 404
+
+
 @app.post("/store")
 def create_store():
     request_data = request.get_json()
@@ -25,4 +33,10 @@ def create_store():
 
 @app.post("/store/<string:name>")
 def create_item(name):
-    ...
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message": "Store not found"}, 404
